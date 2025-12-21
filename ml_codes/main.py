8972e1c79942ac7706e4c120641841040a998db8
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-12-21 00:51:19 +0000

    created cost function

--- ml_codes/main.py
+++ ml_codes/main.py
@@ -10,6 +10,17 @@ class LinearRegression:
     X: np.array
     y: np.array
 
-    def cost_function(self, y_hat: np.array, y_true: np.array = y):
+
+    def cost_function(self, y_hat: np.array, y_true: np.array = y) -> float:
         """cost function for Linear Regression"""
+
+        error = y_hat - y_true
+        squared_error = error**2
+        mean_squared_error = np.mean(squared_error)/2
+        return mean_squared_error
+
+
+    def train(self) -> None:
+        """train Linear Regression method"""
+
         pass
