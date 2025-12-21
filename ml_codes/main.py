113981fdce83826d029efdb01efb89940cc65d9a
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-12-21 01:39:57 +0000

    WIP

--- ml_codes/main.py
+++ ml_codes/main.py
@@ -11,7 +11,7 @@ class LinearRegression:
     y: np.array
 
 
-    def cost_function(self, y_hat: np.array, y_true: np.array = y) -> float:
+    def cost_function(self, y_hat: np.array, y_true: np.array) -> float:
         """cost function for Linear Regression"""
 
         error = y_hat - y_true
@@ -20,11 +20,29 @@ class LinearRegression:
         return mean_squared_error
 
 
-    def train(self, seed: int, learning_rate: float) -> None:
+    def train(self, alpha: float, iterations: int) -> None:
         """train Linear Regression method"""
 
-        np.random.seed(seed=seed)
-        initial_weight, initial_bias = tuple(np.random.randint(1, 100, 2))
-        y_hat = initial_weight*self.X + initial_bias
-        error = self.cost_function(y_hat=y_hat)
+        weights, bias, errors = [], [], []
+        # np.random.seed(seed=seed)
+        initial_weight, initial_bias = 1, 100 # tuple(np.random.randint(1, 100, 2))
+
+        # Gradient Descent
+        # dw = np.mean((y_hat - self.y)*self.X)
+        # db = np.mean(y_hat - self.y)
+
+        for i in range(iterations):
+            y_hat = initial_weight*self.X + initial_bias
+            error = self.cost_function(y_hat=y_hat, y_true=self.y)
+            # initial_weight = initial_weight - alpha*dw
+            # initial_bias = initial_bias - alpha*db
+            # weights.append(initial_weight), bias.append(initial_bias),
+            errors.append(error)
+        return weights, bias, errors
+
+if __name__ == "__main__":
+
+    X=np.array([1,2,5,100,102]); y=np.array([101,102,105,200,202])
+    lr = LinearRegression(X=X, y=y)
+    print(lr.train(0.01, 1))        
         
