11cf58c28be3a3ad1cd612bc602c9473915e2dc5
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-12-21 00:41:07 +0000

    adding codes

--- ml_codes/main.py
+++ ml_codes/main.py
@@ -1,2 +1,15 @@
-def hello():
-    return 'Hello'
+"""Linear Regression module for single variable"""
+
+import numpy as np
+from dataclasses import dataclass
+
+@dataclass
+class LinearRegression:
+    """Linear Regression Class"""
+
+    X: np.array
+    y: np.array
+
+    def cost_function(self, y_hat: np.array, y_true: np.array = y):
+        """cost function for Linear Regression"""
+        pass
