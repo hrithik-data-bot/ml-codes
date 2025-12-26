0c9f2c02ab1da6af934f27905a55028a14bd0112
Hrithik Sharma <sharmahrithik43@gmail.com>
2025-12-26 21:19:03 +0000

    added mse

--- ml_codes/metrics.py
+++ ml_codes/metrics.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 def mean_absolute_error(y_pred: np.array, y_true: np.array) -> float:
-    """mean absolute error"""
+    """mean absolute error for regression"""
 
     return np.mean(y_pred - y_true)
  
 
-def mean_squared_error(y_pred:, y_true: ) -> float:
-    """regression mse"""
-    
+def mean_squared_error(y_pred: np.array, y_true: np.array) -> float:
+    """mean squared error for regression"""
+
+    return np.mean((y_pred - y_true)**2)
